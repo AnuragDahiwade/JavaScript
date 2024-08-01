5db8366ba7b843ed9c0b3a67d4e619b9627a6257
AnuragDahiwade <anurag.dahiwade@gmail.com>
2024-08-01 10:56:16 +0530

    first commit

---
+++ _0_test.js
@@ -0,0 +1,8 @@
+// console.log("Anurag ")
+
+"use strict" // treat all JS code as newer versiion
+
+
+// alert("Hello GG") // will not work in the Node enviroment in the local
+
+
---
+++ basics/01_variables_EMCA.js
@@ -0,0 +1,11 @@
+
+// ---- File: 01_variables_EMCA
+
+// console.log("Anurag ")
+
+"use strict" // treat all JS code as newer versiion
+
+
+// alert("Hello GG") // will not work in the Node enviroment in the local
+
+
