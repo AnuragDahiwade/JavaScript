ddbc60bed9209edbfc4d8975e6b8592576f9c9f3
AnuragDahiwade <anurag.dahiwade@gmail.com>
2024-08-01 12:29:17 +0530

    Data types

--- basics/02_conversionOperation.js
+++ basics/02_conversionOperation.js
@@ -2,18 +2,61 @@
 // --- 02_conversionOperation
 
 // let score = 33
-let score = null
-// let score = undefined
-// let score = true
+// score = null
+// score = undefined
+// score = true
 
-console.log(typeof(score));
-console.log(typeof(score));
+// console.log(typeof(score));
+// console.log(typeof(score));
 
-let intValueInNumber = Number(score)
-console.log(intValueInNumber , typeof(intValueInNumber))
+// let intValueInNumber = Number(score)
+// console.log(intValueInNumber , typeof(intValueInNumber))
 
 
 // intValueInNumber = Number(score + "abc")
 // console.log(intValueInNumber , typeof(intValueInNumber))
 
 
+
+// let isLoggedIn = null
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn, booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "atom" => true
+// undefined => false
+// null => false
+
+// let age = 44
+// let ageString = String(age)
+// console.log(age, typeof(age), "    ", ageString, typeof ageString);
+
+
+
+// ******************* Operations ********************************************
+
+// console.log(4+2);
+// console.log(4-33);
+// console.log(44*34);
+// console.log(3**4);
+// console.log(34/5);
+// console.log(33%6);
+
+
+// let str1 = "Anurag "
+// let str2 = "is Great!!"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2" + 3);
+// console.log(1 + 2 + "3");
+// console.log("1" + "2" + 3);
+
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
