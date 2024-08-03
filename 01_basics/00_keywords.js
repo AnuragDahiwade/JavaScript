8d78e3f072f4d80655b2748d2a24836d3a8299c9
AnuragDahiwade <anurag.dahiwade@gmail.com>
2024-08-03 12:47:57 +0530

    Dom

---
+++ 01_basics/00_keywords.js
@@ -0,0 +1,8 @@
+// console.log("Anurag ")
+
+"use strict" // treat all JS code as newer versiion
+
+
+// alert("Hello GG") // will not work here on the local node environment
+
+
--- _0_test.js
+++
@@ -1,8 +0,0 @@
-// console.log("Anurag ")
-
-"use strict" // treat all JS code as newer versiion
-
-
-// alert("Hello GG") // will not work here on the local node environment
-
-
